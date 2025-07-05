dd8fcbff94c9fcf5f630093d3c6864300588f645
Peter <peter@reidweb.com>
2025-07-05 18:14:21 +0100

    attempt to configure target account role i can assume

--- target-account/bin/target-account.ts
+++ target-account/bin/target-account.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { TargetAccountStack } from '../lib/target-account-stack';
+import { TargetAccountStack } from '../lib/target_account_stack';
 
 const app = new cdk.App();
 new TargetAccountStack(app, 'TargetAccountStack', {
